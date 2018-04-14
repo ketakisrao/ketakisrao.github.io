bf5c11848cee082f2b231d708d851307e941efc1
Ketaki Rao <ketakisrao@Ketakis-MBP.Dlink>
2018-04-14 20:41:44 +0530

    Origami page revamp

--- js/jsGameScript.js
+++ js/jsGameScript.js
@@ -1,6 +1,7 @@
 $(function(){
     var typed = new Typed('#whack', {
     strings: ["A simple JavaScript game where in you need to whack moles coming out of their holes, but you need to hurry since you've got only 30s! Beat my highest score of 63. The game is mobile, tablet and PC compatible."],
+    typeSpeed: 10,
     backSpeed: 0,
     cursorChar: ''
   });
@@ -16,6 +17,7 @@ $(document).ready(function() {
                 var write = $(this).attr('data-wri');
                 var typed = new Typed('#' + id, {
                     strings: [write],
+                    typeSpeed: 10,
                     backSpeed: 0,
                     cursorChar: ''
                 });
