64f72d6a48c9372ef71b9c4a1ad16782c30e0c36
ketakiionos <ketaki@ionosnetworks.com>
2018-09-08 11:36:33 +0530

    Added sticky header

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 var app = angular.module('myApp', []);
 app.controller('myController', ["$scope", "$timeout", function ($scope, $timeout) {
     var init = function () {
+        var description = "<b class=\"title\">KETAKI RAO</b><br> UI developer & Designer"
         var poster = document.getElementById("poster");
-        var html = "<div class=\"content\">I am a front-end developer</div>";
-        for (i = 1; i <= 66; i++) {
+        var html = "<div class=\"content\">" + description + "</div>";
+        for (i = 1; i <= 63; i++) {
             html += '<img src="MyIcons/pi' + i + '.svg" />\n';
         }
         // console.log(html);
