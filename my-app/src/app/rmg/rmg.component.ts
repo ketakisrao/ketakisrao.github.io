1b5e2073b7f160c418cdb4f455a8f96bd5275ab9
ketakiionos <ketaki@ionosnetworks.com>
2018-09-24 22:56:41 +0530

    LRM page

---
+++ my-app/src/app/rmg/rmg.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rmg',
+  templateUrl: './rmg.component.html',
+  styleUrls: ['./rmg.component.css']
+})
+export class RmgComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
