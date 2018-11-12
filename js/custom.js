05a8900b7b4b5750680532ea751698ecfb26df88
ketakiionos <ketaki@ionosnetworks.com>
2018-11-12 22:32:16 +0530

    Added timeline and tag cloud

--- js/custom.js
+++ js/custom.js
@@ -1,22 +1,41 @@
-
-
 $(document).ready(function () {
+        if (!$("#skillsCanvas").tagcanvas({
+                textColour: '#888888',
+                outlineThickness: 0.5,
+                maxSpeed: 0.03,
+                depth: 0.3,
+                wheelZoom: false,
+                pinchZoom: false,
+                noSelect: true
+        })) {
+                console.log("Tag Cloud not initialised");
+        }
+});
 
-        /* Every time the window is scrolled ... */
-        $(window).scroll(function () {
+/* Check the location of each element */
+$('.content').each(function (i) {
 
-                /* Check the location of each desired element */
-                $('.hideme').each(function (i) {
+        var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+        var bottom_of_window = $(window).height();
+        console.log(bottom_of_object);
+        console.log(bottom_of_window);
 
-                        var bottom_of_object = $(this).position().top + $(this).outerHeight();
-                        var bottom_of_window = $(window).scrollTop() + $(window).height();
+        if (bottom_of_object > bottom_of_window) {
+                $(this).addClass('hidden');
+        }
+});
 
-                        /* If the object is completely visible in the window, fade it it */
-                        if (bottom_of_window > bottom_of_object) {
 
-                                $(this).animate({ 'opacity': '1' }, 1500);
+$(window).scroll(function () {
+        /* Check the location of each element hidden */
+        $('.hidden').each(function (i) {
 
-                        }
-                });
+                var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+                var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+                /* If the object is completely visible in the window, fadeIn it */
+                if (bottom_of_window > bottom_of_object) {
+                        $(this).animate({ 'opacity': '1' }, 1700);
+                }
         });
 });
