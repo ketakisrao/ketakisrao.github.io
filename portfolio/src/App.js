3bb544249acdf168225da9931fe75e6325ff0bb8
ketakisrao <ketaki_8@hotmail.com>
2020-07-19 22:25:28 -0400

    Scroll to top

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -6,6 +6,7 @@ import {
     Switch,
     Route
 } from "react-router-dom";
+import ScrollToTop from './Components/ScrollToTop';
 import About from './About/';
 import Portfolio from './Portfolio/';
 import EDU from './EDU/';
@@ -16,21 +17,22 @@ function App() {
     return (
         <div className="App">
             <Router>
-                <ButtonAppBar></ButtonAppBar>
-                <div className="content">
-                    <Switch>
-                        <Route exact path="/" component={Portfolio}>
-                        </Route>
-                        <Route path="/about" component={About}>
-                        </Route>
-                        <Route path="/EDU" component={EDU}>
-                        </Route>
-                        <Route path="/PIT" component={PIT}>
-                        </Route>
-                        <Route path="/LRM" component={LRM}>
-                        </Route>
-                    </Switch>
-                </div>
+                <ScrollToTop />
+                    <ButtonAppBar></ButtonAppBar>
+                    <div className="content">
+                        <Switch>
+                            <Route exact path="/" component={Portfolio}>
+                            </Route>
+                            <Route path="/about" component={About}>
+                            </Route>
+                            <Route path="/EDU" component={EDU}>
+                            </Route>
+                            <Route path="/PIT" component={PIT}>
+                            </Route>
+                            <Route path="/LRM" component={LRM}>
+                            </Route>
+                        </Switch>
+                    </div>
             </Router>
         </div>
     );
